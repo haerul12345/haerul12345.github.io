f1ff214b14116c987724eac494fe55d35d4a4229
haerul12345 <haerul.jusmar@hotmail.com>
2025-07-27 10:59:41 +0700

    define alert icon

--- script.js
+++ script.js
@@ -138,6 +138,39 @@ function copyTable() {
   }
 }
 
+// Alert Icon def
+const alertIcons = {
+  error: `<div style="font-size: 2.5em; text-align: center;">  
+	<svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+	  <line x1="18" y1="6" x2="6" y2="18" />
+	  <line x1="6" y1="6" x2="18" y2="18" />
+	</svg>
+  </div>`,
+  
+  success: `<div style="font-size: 2.5em; text-align: center;">
+	  <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24" fill="none" stroke="green" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+	  <polyline points="20 6 9 17 4 12" />
+	  </svg>
+  </div>`,
+  
+  warning: `<div style="font-size: 2.5em; text-align: center;">  
+		
+	<svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24" fill="none" stroke="orange" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+	  <path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z" />
+	  <line x1="12" y1="9" x2="12" y2="13" />
+	  <line x1="12" y1="17" x2="12.01" y2="17" />
+	</svg>
+  </div>`,
+  
+  info: `<div style="font-size: 2.5em; text-align: center;">
+	<svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24" fill="none" stroke="blue" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+		<circle cx="12" cy="12" r="10"/>
+		<line x1="12" y1="7" x2="12" y2="13"/>
+		<line x1="12" y1="17" x2="12.01" y2="17"/>
+	</svg>
+  </div>`
+};
+
 // Alert Message function
 function showAlert(message, type = 'info') {
   const icon = alertIcons[type] || alertIcons.info;
