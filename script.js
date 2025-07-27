e7837244761cb91a3e4cc75ddd236ac28975838f
haerul12345 <haerul.jusmar@hotmail.com>
2025-07-27 11:36:03 +0700

    add DE22 Parser

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  // Input EventListener
+  // AXL Input EventListener
   const input = document.getElementById("json-input");
   if (input) {
     input.addEventListener("input", parseAXL);
@@ -31,7 +31,12 @@ document.addEventListener("DOMContentLoaded", function () {
     okButton.addEventListener('click', closeAlert);
   }
 
-
+  // DE22 Input EventListener
+  const de22Input = document.getElementById('de22-input');
+  if (de22Input) {
+    de22Input.addEventListener('input', displayParsedData);
+  }
+  
 });
 
 // Screen handling
@@ -189,4 +194,182 @@ function showAlert(message, type = 'info') {
 function closeAlert() {
   document.getElementById('customAlert').style.display = 'none';
   document.getElementById("overlay").style.display = "none";
+}
+
+// DE22 Parser code
+function displayParsedData() {
+  const cardData = document.getElementById('de22-input').value;
+
+  if (!cardData.trim()) {
+    alert("DE22 data is empty. Please enter the data.");
+    return;
+  }
+
+  const parsedData = parseCardData(cardData);
+  let output = '';
+  for (const [key, value] of Object.entries(parsedData)) {
+    output += `${key} > ${value}\n`;
+  }
+  document.getElementById('parse-output').value = output;
+  resizeparsedDataOutput();
+}
+
+function parseCardData(cardData) {
+  const definitions = {
+    cardDataInputCapability: {
+      '0': 'Unknown',
+      '1': 'Manual, no terminal',
+      '2': 'Magnetic stripe read',
+      '3': 'Bar code',
+      '4': 'OCR',
+      '5': 'Integrate circuit card (ICC)',
+      '6': 'Key entered',
+      'A': 'Contactless ICC',
+      'B': 'Contactless magnetic stripe'
+    },
+    cardholderAuth: {
+      '0': 'No electronic authentication',
+      '1': 'PIN',
+      '2': 'Electronic signature analysis',
+      '3': 'Biometrics',
+      '4': 'Biographic',
+      '5': 'Electronic authentication inoperative',
+      '6': 'Other'
+    },
+    cardCapture: {
+      '0': 'None',
+      '1': 'Capture'
+    },
+    operatingEnvironment: {
+      '0': 'No terminal used',
+      '1': 'On premises of card acceptor, attended',
+      '2': 'On premises of card acceptor, unattended',
+      '3': 'Off premises of card acceptor, attended',
+      '4': 'Off premises of card acceptor, unattended',
+      '5': 'On premises of cardholder, attended'
+    },
+    cardholderPresent: {
+      '0': 'Cardholder present',
+      '1': 'Cardholder not present, unspecified',
+      '2': 'Cardholder not present, mail order',
+      '3': 'Cardholder not present, telephone',
+      '4': 'Cardholder not present, standing authorisation'
+    },
+    cardPresent: {
+      '0': 'Card not present',
+      '1': 'Card present'
+    },
+    cardDataInputMode: {
+      '0': 'Unspecified',
+      '1': 'Manual, no terminal',
+      '2': 'Magnetic stripe read',
+      '3': 'Bar code',
+      '4': 'OCR',
+      '5': 'Integrate circuit card (ICC)',
+      '6': 'Key entered',
+      '7': 'Contactless ICC',
+      '8': 'Contactless magnetic stripe'
+    },
+    cardholderAuthMethod: {
+      '0': 'Not authenticated',
+      '1': 'PIN',
+      '2': 'Electronic signature analysis',
+      '3': 'Biometrics',
+      '4': 'Biographic',
+      '5': 'Manual signature verification',
+      '6': 'Other manual verification'
+    },
+    cardholderAuthEntity: {
+      '0': 'Not authenticated',
+      '1': 'Integrated circuit card',
+      '2': 'Terminal',
+      '3': 'Authorising agent',
+      '4': 'Merchant',
+      '5': 'Other'
+    },
+    cardDataOutput: {
+      '0': 'Unknown',
+      '1': 'None',
+      '2': 'Magnetic stripe write',
+      '3': 'Integrate circuit card (ICC)'
+    },
+    terminalOutput: {
+      '0': 'Unknown',
+      '1': 'None',
+      '2': 'Printing',
+      '3': 'Display',
+      '4': 'Printing and display'
+    },
+    pinCapture: {
+      '0': 'No PIN capture capability',
+      '1': 'Device PIN capture capability unknown',
+      '4': 'Four characters',
+      '5': 'Five characters',
+      '6': 'Six characters',
+      '7': 'Seven characters',
+      '8': 'Eight characters',
+      '9': 'Nine characters',
+      'A': 'Ten characters',
+      'B': 'Eleven characters',
+      'C': 'Twelve characters'
+    },
+    terminalOperator: {
+      '0': 'Customer operated',
+      '1': 'Card acceptor operated',
+      '2': 'Administrative'
+    },
+    terminalType: {
+      '00': 'Administrative terminal',
+      '01': 'POS terminal',
+      '02': 'ATM',
+      '03': 'Home terminal',
+      '04': 'Electronic cash register (ECR)',
+      '05': 'Dial terminal',
+      '06': 'Travellers check machine',
+      '07': 'Fuel machine',
+      '08': 'Scrip machine',
+      '09': 'Coupon machine',
+      '10': 'Ticket machine',
+      '11': 'Point-of-Banking terminal',
+      '12': 'Teller',
+      '13': 'Franchise teller',
+      '14': 'Personal banking',
+      '15': 'Public utility',
+      '16': 'Vending',
+      '17': 'Self-service',
+      '18': 'Authorization',
+      '19': 'Payment',
+      '20': 'VRU',
+      '21': 'Smart phone',
+      '22': 'Interactive television',
+      '23': 'Personal digital assistant',
+      '24': 'Screen phone',
+      '90': 'E-commerce - No encryption; no authentication',
+      '91': 'E-commerce - SET/3D-Secure encryption; cardholder certificate not used (non-authenticated)',
+      '92': 'E-commerce - SET/3D-Secure encryption; cardholder certificate used (authenticated)',
+      '93': 'E-commerce - SET encryption, chip cryptogram used; cardholder certificate not used',
+      '94': 'E-commerce - SET encryption, chip cryptogram used; cardholder certificate used',
+      '95': 'Channel encryption (SSL); cardholder certificate not used (non-authenticated)',
+      '96': 'E-commerce - Channel encryption (SSL); chip cryptogram used, cardholder certificate not used'
+    }
+  };
+
+  const parsedData = {
+    'Pos 1 ': `(${cardData[0]}) ${definitions.cardDataInputCapability[cardData[0]]}`,
+    'Pos 2 ': `(${cardData[1]}) ${definitions.cardholderAuth[cardData[1]]}`,
+    'Pos 3 ': `(${cardData[2]}) ${definitions.cardCapture[cardData[2]]}`,
+    'Pos 4 ': `(${cardData[3]}) ${definitions.operatingEnvironment[cardData[3]]}`,
+    'Pos 5 ': `(${cardData[4]}) ${definitions.cardholderPresent[cardData[4]]}`,
+    'Pos 6 ': `(${cardData[5]}) ${definitions.cardPresent[cardData[5]]}`,
+    'Pos 7 ': `(${cardData[6]}) ${definitions.cardDataInputMode[cardData[6]]}`,
+    'Pos 8 ': `(${cardData[7]}) ${definitions.cardholderAuthMethod[cardData[7]]}`,
+    'Pos 9 ': `(${cardData[8]}) ${definitions.cardholderAuthEntity[cardData[8]]}`,
+    'Pos 10': `(${cardData[9]}) ${definitions.cardDataOutput[cardData[9]]}`,
+    'Pos 11': `(${cardData[10]}) ${definitions.terminalOutput[cardData[10]]}`,
+    'Pos 12': `(${cardData[11]}) ${definitions.pinCapture[cardData[11]]}`,
+    'Pos 13': `(${cardData[12]}) ${definitions.terminalOperator[cardData[12]]}`,
+    'Pos 14-15': `(${cardData.substring(13, 15)}) ${definitions.terminalType[cardData.substring(13, 15)]}`
+  };
+
+  return parsedData;
 }
