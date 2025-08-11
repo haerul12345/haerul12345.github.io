132e7b620d21aa3e5ad2099a31154f1cc75bacf8
haerul12345 <haerul.jusmar@hotmail.com>
2025-08-11 16:29:33 +0700

    hide app name

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Application version
   const appVersion = "3.0";
-  document.getElementById("app-version").textContent = `ParserSweet Version: ${appVersion} © 2025 hji`;
+  //document.getElementById("app-version").textContent = `ParserSweet Version: ${appVersion} © 2025 hji`;
+  document.getElementById("app-version").textContent = `Version: ${appVersion} © 2025 hji`;
 
   // Button EventListener
   const buttons = document.querySelectorAll(".btn");
